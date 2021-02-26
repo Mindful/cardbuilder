cc2109253c669ae5c84d5e94c0395cd9b1ee0fe6
Joshua Tanner <mindful.jt@gmail.com>
2021-02-26 20:20:52 +0900

    final draft packaging

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     author="Joshua Tanner",
     author_email="mindful.jt@gmail.com",
     description="A package for programmatically generating language learning flashcards",
+    keywords="flashcards anki language learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Mindful/cardbuilder",
@@ -19,4 +20,13 @@ setuptools.setup(
     ],
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
+    install_requires=[
+        "requests ~= 2.25.1",
+        "lxml ~= 4.6.2",
+        "genanki ~= 0.10.1",
+        "tqdm ~= 4.56.0",
+        "pykakasi ~= 2.0.6",
+        "unidic-lite ~= 1.0.8",
+        "fugashi ~= 1.1.0"
+    ]
 )
