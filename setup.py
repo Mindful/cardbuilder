aa5da3822b8d73b8f4470cd7ded71d0bf157682a
Joshua Tanner <mindful.jt@gmail.com>
2021-06-05 22:38:56 +0900

    update html, bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cardbuilder",
-    version="0.0.25",
+    version="0.0.26",
     author="Joshua Tanner",
     author_email="mindful.jt@gmail.com",
     description="A package for programmatically generating language learning flashcards",
